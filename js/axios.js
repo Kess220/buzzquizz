d623939f1677d48f28ed48dfda4f96458e49b2fe
Adryelgcouto <adryelgcouto@gmail.com>
2023-04-20 21:07:29 -0300

    adicionado limitação de level

--- js/axios.js
+++ js/axios.js
@@ -1,73 +1,76 @@
-axios.defaults.headers.common["Authorization"] = "mIsztIcWVzidlM4aidMUviCe";
+axios.defaults.headers.common['Authorization'] = 'mIsztIcWVzidlM4aidMUviCe'
 
 // Buscar Quizz
 function buscarQuizz() {
   axios
-    .get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes")
-    .then((response) => {
-      console.log(response.data);
+    .get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes')
+    .then(response => {
+      console.log(response.data)
     })
-    .catch((error) => {
-      console.error(error);
-    });
+    .catch(error => {
+      console.error(error)
+    })
 }
 
-buscarQuizz();
+buscarQuizz()
 
 // Buscar Quizz por ID
 
 function buscarQuizzID(ID) {
   axios
     .get(`https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/${ID}`)
-    .then((response) => {
-      console.log(response.data);
+    .then(response => {
+      console.log(response.data)
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .catch(error => {
+      console.log(error)
+    })
 }
-buscarQuizzID(2);
+buscarQuizzID(2)
 
 let quizzNovo = axios
-  .get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes")
-  .then((response) => {
-    console.log(response.data);
+  .get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes')
+  .then(response => {
+    console.log(response.data)
   })
-  .catch((error) => {
-    console.log(error);
-  });
+  .catch(error => {
+    console.log(error)
+  })
 
 function buscarInfo() {
-  const title = document.getElementById("title").value;
-  const urlImg = document.getElementById("urlImg").value;
-  const numPerg = document.getElementById("numPerg").value;
-  const level = document.getElementById("level").value;
-
-  console.log(title, urlImg, numPerg, level);
+  const title = document.getElementById('title').value
+  const urlImg = document.getElementById('urlImg').value
+  const numPerg = document.getElementById('numPerg').value
+  const level = document.getElementById('level').value
+  if (numPerg > 4) {
+    alert('esse numero é muito alto')
+  }
+  
+  console.log(title, urlImg, numPerg, level)
 }
 
 function criePerguntas() {
   // Pergunta1
-  const texto1 = document.getElementById("texto-pergunta").value;
-  const corPerg1 = document.getElementById("corPerg").value;
-  const perg1 = [texto1, corPerg1];
-  console.log(perg1);
+  const texto1 = document.getElementById('texto-pergunta').value
+  const corPerg1 = document.getElementById('corPerg').value
+  const perg1 = [texto1, corPerg1]
+  console.log(perg1)
   // Resposta correta
-  const respostaCorreta = document.getElementById("res-correct").value;
-  const urlCorreta = document.getElementById("url-correct").value;
-  const correta = [respostaCorreta, urlCorreta];
+  const respostaCorreta = document.getElementById('res-correct').value
+  const urlCorreta = document.getElementById('url-correct').value
+  const correta = [respostaCorreta, urlCorreta]
 
-  console.log(correta);
+  console.log(correta)
 
   //Resposta incorretas
-  const respostaIncorreta1 = document.getElementById("res-in1").value;
-  const urlIncorreta1 = document.getElementById("url-in1").value;
+  const respostaIncorreta1 = document.getElementById('res-in1').value
+  const urlIncorreta1 = document.getElementById('url-in1').value
 
-  const respostaIncorreta2 = document.getElementById("res-in2").value;
-  const urlIncorreta2 = document.getElementById("url-in2").value;
+  const respostaIncorreta2 = document.getElementById('res-in2').value
+  const urlIncorreta2 = document.getElementById('url-in2').value
 
-  const respostaIncorreta3 = document.getElementById("res-in3").value;
-  const urlIncorreta3 = document.getElementById("url-in3").value;
+  const respostaIncorreta3 = document.getElementById('res-in3').value
+  const urlIncorreta3 = document.getElementById('url-in3').value
 
   console.log(
     respostaIncorreta1,
@@ -76,6 +79,6 @@ function criePerguntas() {
     urlIncorreta2,
     respostaIncorreta3,
     urlIncorreta3
-  );
+  )
   // Pergunta 2
 }
