b23883c2a9667c530d287d7a0ed1ec9c87da8df5
Kess220 <prodvictorkaio@gmail.com>
2023-04-21 01:15:05 -0300

    refazendo o busca de info

--- js/axios.js
+++ js/axios.js
@@ -1,131 +1,131 @@
-axios.defaults.headers.common['Authorization'] = 'mIsztIcWVzidlM4aidMUviCe'
+axios.defaults.headers.common["Authorization"] = "mIsztIcWVzidlM4aidMUviCe";
 
 // Buscar Quizz
 function buscarQuizz() {
   axios
-    .get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes')
-    .then(response => {
-      console.log(response.data)
+    .get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes")
+    .then((response) => {
+      console.log(response.data);
     })
-    .catch(error => {
-      console.error(error)
-    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-buscarQuizz()
+buscarQuizz();
 
 // Buscar Quizz por ID
 
 function buscarQuizzID(ID) {
   axios
     .get(`https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/${ID}`)
-    .then(response => {
-      console.log(response.data)
+    .then((response) => {
+      console.log(response.data);
     })
-    .catch(error => {
-      console.log(error)
-    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-buscarQuizzID(2)
+buscarQuizzID(2);
 
 let quizzNovo = axios
-  .get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes')
-  .then(response => {
-    console.log(response.data)
+  .get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes")
+  .then((response) => {
+    console.log(response.data);
   })
-  .catch(error => {
-    console.log(error)
-  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 function buscarInfo() {
-  const title = document.getElementById('title').value
-  const urlImg = document.getElementById('urlImg').value
-  const numPerg = document.getElementById('numPerg').value
-  const level = document.getElementById('level').value
+  const title = document.getElementById("title").value;
+  const urlImg = document.getElementById("urlImg").value;
+  const numPerg = document.getElementById("numPerg").value;
+  const level = document.getElementById("level").value;
   if (numPerg > 4) {
-    alert('esse numero é muito alto')
+    alert("esse numero é muito alto");
   }
 
-  console.log(title, urlImg, numPerg, level)
+  console.log(title, urlImg, numPerg, level);
   obj = {
     title: title,
     image: urlImg,
     questions: [
       {
-        title: 'Título da pergunta 1',
-        color: '#123456',
+        title: "Título da pergunta 1",
+        color: "#123456",
         answers: [
           {
-            text: 'Texto da resposta 1',
-            image: 'https://http.cat/411.jpg',
-            isCorrectAnswer: true
+            text: "Texto da resposta 1",
+            image: "https://http.cat/411.jpg",
+            isCorrectAnswer: true,
           },
           {
-            text: 'Texto da resposta 2',
-            image: 'https://http.cat/412.jpg',
-            isCorrectAnswer: false
-          }
-        ]
+            text: "Texto da resposta 2",
+            image: "https://http.cat/412.jpg",
+            isCorrectAnswer: false,
+          },
+        ],
       },
       {
-        title: 'Título da pergunta 2',
-        color: '#123456',
+        title: "Título da pergunta 2",
+        color: "#123456",
         answers: [
           {
-            text: 'Texto da resposta 1',
-            image: 'https://http.cat/411.jpg',
-            isCorrectAnswer: true
+            text: "Texto da resposta 1",
+            image: "https://http.cat/411.jpg",
+            isCorrectAnswer: true,
           },
           {
-            text: 'Texto da resposta 2',
-            image: 'https://http.cat/412.jpg',
-            isCorrectAnswer: false
-          }
-        ]
+            text: "Texto da resposta 2",
+            image: "https://http.cat/412.jpg",
+            isCorrectAnswer: false,
+          },
+        ],
       },
       {
-        title: 'Título da pergunta 3',
-        color: '#123456',
+        title: "Título da pergunta 3",
+        color: "#123456",
         answers: [
           {
-            text: 'Texto da resposta 1',
-            image: 'https://http.cat/411.jpg',
-            isCorrectAnswer: true
+            text: "Texto da resposta 1",
+            image: "https://http.cat/411.jpg",
+            isCorrectAnswer: true,
           },
           {
-            text: 'Texto da resposta 2',
-            image: 'https://http.cat/412.jpg',
-            isCorrectAnswer: false
-          }
-        ]
-      }
+            text: "Texto da resposta 2",
+            image: "https://http.cat/412.jpg",
+            isCorrectAnswer: false,
+          },
+        ],
+      },
     ],
     levels: [
       {
-        title: 'Título do nível 1',
-        image: 'https://http.cat/411.jpg',
-        text: 'Descrição do nível 1',
-        minValue: 0
+        title: "Título do nível 1",
+        image: "https://http.cat/411.jpg",
+        text: "Descrição do nível 1",
+        minValue: 0,
       },
       {
-        title: 'Título do nível 2',
-        image: 'https://http.cat/412.jpg',
-        text: 'Descrição do nível 2',
-        minValue: 50
-      }
-    ]
-  }
+        title: "Título do nível 2",
+        image: "https://http.cat/412.jpg",
+        text: "Descrição do nível 2",
+        minValue: 50,
+      },
+    ],
+  };
 }
 function gerarPergunta() {
   // pega elemento
-  const paginaCriancaoQuiz = document.querySelector('.paginaCriancaoQuiz')
-  paginaCriancaoQuiz.classList.add('invisivel')
-  const criarPerguntas = document.querySelector('.criar-perguntas')
-  criarPerguntas.classList.remove('invisivel')
-  const numPerg = document.getElementById('numPerg').value
+  const paginaCriancaoQuiz = document.querySelector(".paginaCriancaoQuiz");
+  paginaCriancaoQuiz.classList.add("invisivel");
+  const criarPerguntas = document.querySelector(".criar-perguntas");
+  criarPerguntas.classList.remove("invisivel");
+  const numPerg = document.getElementById("numPerg").value;
   //pega a quantidade
   for (let i = 1; i <= numPerg; i++) {
-    const criarPergunta = document.querySelector('.criar-pergunta')
+    const criarPergunta = document.querySelector(".criar-pergunta");
     // pega a div que embloba tudo
     criarPergunta.innerHTML += `
     <div class="perguntas">
@@ -147,54 +147,23 @@ function gerarPergunta() {
     
     
     
-    `
+    `;
     // renderizar o html
   }
   // fazer um loop para criar tantas perguntas que o cliente desejá
+
+  // Buscando Info para colocar no OBJCT
 }
 
-function criePerguntas() {
-  // Pergunta1
-  const texto1 = document.getElementById('texto-pergunta').value
-  const corPerg1 = document.getElementById('corPerg').value
-  const perg1 = [texto1, corPerg1]
-  // console.log(perg1);
-  // Resposta correta
-  const respostaCorreta = document.getElementById('res-correct').value
-  const urlCorreta = document.getElementById('url-correct').value
-  const correta = [respostaCorreta, urlCorreta]
+// let answers = []
 
-  // console.log(correta);
-
-  //Resposta incorretas
-  const respostaIncorreta1 = document.getElementById('res-in1').value
-  const urlIncorreta1 = document.getElementById('url-in1').value
-
-  const respostaIncorreta2 = document.getElementById('res-in2').value
-  const urlIncorreta2 = document.getElementById('url-in2').value
-
-  const respostaIncorreta3 = document.getElementById('res-in3').value
-  const urlIncorreta3 = document.getElementById('url-in3').value
-
-  // console.log(
-  //   respostaIncorreta1,
-  //   urlIncorreta1,
-  //   respostaIncorreta2,
-  //   urlIncorreta2,
-  //   respostaIncorreta3,
-  //   urlIncorreta3
-  // );
-
-  let answers = []
-
-  for (let i = 1; i <= 5; i++) {
-    let perguntaCorreta = i == 1
-    let answer = {
-      text: texto1,
-      image: corPerg1,
-      perguntaCorreta: perguntaCorreta
-    }
-    answers.push(answer)
-  }
-  console.log(answers)
-}
+// for (let i = 1; i <= 5; i++) {
+//   let perguntaCorreta = i == 1
+//   let answer = {
+//     text: texto1,
+//     image: corPerg1,
+//     perguntaCorreta: perguntaCorreta
+//   }
+//   answers.push(answer)
+// }
+// console.log(answers)
