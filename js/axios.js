ca9a70dcf1cef6749d603989fc57c91cea506306
Adryelgcouto <adryelgcouto@gmail.com>
2023-04-24 17:39:17 -0300

    adicionado data test

--- js/axios.js
+++ js/axios.js
@@ -16,7 +16,7 @@ function meusQuizzes() {
   const titlePronto = JSON.parse(title)
   const imgPronto = JSON.parse(img)
   console.log(img, title, id)
-  if (localStorage.length > 0) {
+  if (localStorage.length  > 0) {
     for (
       let i = 0;
       i < titlePronto.length && imgPronto.length && idPronto.length;
@@ -24,7 +24,7 @@ function meusQuizzes() {
     ) {
       const tudoDentro = document.querySelector('.tudoDentro')
       tudoDentro.innerHTML += `
-    <div class="quizzes" >
+    <div data-test="my-quiz" class="quizzes" >
         <img onclick='gerarQuizz(${Meuid})' src="${imgPronto[i]}" alt="" />
         <div>
           <p>${titlePronto[i]}</p>
@@ -57,7 +57,7 @@ function todosOsQuizzes() {
         const image = response.data[i].image
         Meuid = response.data[i].id
         todosOsQuizzesRenderizar.innerHTML += `
-        <div class="quizzes ${Meuid}" >
+        <div data-test="others-quiz" class="quizzes ${Meuid}" >
           <img onclick="buscarQuizzID(${Meuid})" src="${image}" alt="" />
           <div>
             <p>${title}</p>
@@ -101,6 +101,7 @@ function buscarQuizzID(ID) {
       const cabeca = document.createElement('div')
       cabeca.setAttribute('class', 'cabeca')
       quizzAcerto.appendChild(cabeca)
+      cabeca.setAttribute("data-test", "banner")
       cabeca.innerHTML += `
   <img src="${promisse.data.image}" alt="">
   <p>${promisse.data.title}</p>
@@ -116,31 +117,29 @@ function buscarQuizzID(ID) {
           `${promisse.data.questions[i].answers[i].image} oi oi oi oi oi oi oi `
         )
         contentPergunta.innerHTML += `
-      <div class="tudoPergunta">
-              <div class="perguntaQuizz" style="background-color: ${promisse.data.questions[i].color}">
-                <p>${promisse.data.questions[i].title}</p>
-              </div>
-              <div class="resposta ">
-                <div>
-                  <img src="${promisse.data.questions[i].answers[0].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[0].text}</p>
-                </div>
-                <div>
-                  <img src="${promisse.data.questions[i].answers[1].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[1].text}</p>
-                </div>
-                <div>
-                  <img src="${promisse.data.questions[i].answers[2].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[2].text}</p>
-                </div>
-                <div>
-                  <img src="${promisse.data.questions[i].answers[3].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[3].text}</p>
-                </div>
-              </div>
-            </div>
-      
-      
+        <div data-test="question" class="tudoPergunta">
+        <div data-test="question-title"   class="perguntaQuizz" style="background-color: ${promisse.data.questions[i].color}">
+          <p>${promisse.data.questions[i].title}</p>
+        </div>
+        <div class="resposta ">
+          <div data-test="answer">
+            <img src="${promisse.data.questions[i].answers[0].image}" alt="">
+            <p data-test="answer-text">${promisse.data.questions[i].answers[0].text}</p>
+          </div>
+          <div data-test="answer">
+            <img src="${promisse.data.questions[i].answers[1].image}" alt="">
+            <p data-test="answer-text">${promisse.data.questions[i].answers[1].text}</p>
+          </div>
+          <div data-test="answer">
+            <img src="${promisse.data.questions[i].answers[2].image}" alt="">
+            <p data-test="answer-text">${promisse.data.questions[i].answers[2].text}</p>
+          </div>
+          <div data-test="answer" >
+            <img src="${promisse.data.questions[i].answers[3].image}" alt="">
+            <p data-test="answer-text" >${promisse.data.questions[i].answers[3].text}</p>
+          </div>
+        </div>
+      </div>
       `
       }
     })
@@ -205,20 +204,20 @@ function gerarPergunta() {
       const criarPergunta = document.querySelector('.criar-pergunta')
       // pega a div que embloba tudo
       criarPergunta.innerHTML += `
-    <div class="perguntas">
+    <div data-test="question-ctn"  class="perguntas">
           <p  class="pergunta">Pergunta ${i}</p>
-          <input id="textoPergunta${i}" type="text" placeholder="Texto da pergunta" name="title_${i}">
-          <input id="CorPergunta${i}" type="text" placeholder="Cor de fundo da pergunta" name="color_${i}">
+          <input data-test="question-input" id="textoPergunta${i}" type="text" placeholder="Texto da pergunta" name="title_${i}">
+          <input data-test="question-color-input" id="CorPergunta${i}" type="text" placeholder="Cor de fundo da pergunta" name="color_${i}">
           <p class="respostaCorreta">Resposta Correta</p>
-          <input id="respostaCorreta${i}" type="text" placeholder="Resposta correta" name="text_T_${i}">
-          <input id="cachorroMijando${i}" type="url " placeholder="URL da imagem" name="image_T_${i}">
+          <input data-test="correct-answer-input" id="respostaCorreta${i}" type="text" placeholder="Resposta correta" name="text_T_${i}">
+          <input data-test="correct-img-input"   id="cachorroMijando${i}" type="url " placeholder="URL da imagem" name="image_T_${i}">
           <p class="respostaIncorreta">Respostas incorretas</p>
-          <input id="primeiraRespostaIncorreta${i}" type="text" placeholder="Resposta incorreta 1" name="text1_${i}">
-          <input id="urlPrimeiraRespostaIncorreta${i}" type="url" placeholder="URL da imagem 1" name="image1_${i}">
-          <input id="segundaRespostaIncorreta${i}" type="text" placeholder="Resposta incorreta 2" name="text2_${i}">
-          <input id="urlSegundaRespostaIncorreta${i}" type="url " placeholder="URL da imagem 2" name="image2_${i}">
-          <input  id="terceiraRespostaIncorreta${i}" type="text" placeholder="Resposta incorreta 3" name="text3_${i}">
-          <input id="urlTerceiraRespostaIncorreta${i}" type="url" placeholder="URL da imagem 3"name="image3_${i}">
+          <input data-test="wrong-answer-input"  id="primeiraRespostaIncorreta${i}" type="text" placeholder="Resposta incorreta 1" name="text1_${i}">
+          <input data-test="wrong-img-input" id="urlPrimeiraRespostaIncorreta${i}" type="url" placeholder="URL da imagem 1" name="image1_${i}">
+          <input data-test="wrong-answer-input" id="segundaRespostaIncorreta${i}" type="text" placeholder="Resposta incorreta 2" name="text2_${i}">
+          <input data-test="wrong-img-input" id="urlSegundaRespostaIncorreta${i}" type="url " placeholder="URL da imagem 2" name="image2_${i}">
+          <input  data-test="wrong-answer-input" id="terceiraRespostaIncorreta${i}" type="text" placeholder="Resposta incorreta 3" name="text3_${i}">
+          <input data-test="wrong-img-input" id="urlTerceiraRespostaIncorreta${i}" type="url" placeholder="URL da imagem 3"name="image3_${i}">
     </div>
     
     
@@ -278,12 +277,13 @@ algumaCoisa.addEventListener('submit', e => {
   for (let i = 1; i <= level; i++) {
     const gerarNiveis = document.querySelector('.gerarNiveis')
     gerarNiveis.innerHTML += `
-    <div class="segura-nivel">
+    <div data-test="level-ctn"  class="segura-nivel">
         <p>Nível ${i}</p>
-        <input id='tituloNivel' type="text" placeholder="Título do nível" name='tituloNivel${i}' >
-        <input type="text" placeholder="% de acerto mínima" name='porcentagemAcertos${i}' >
-        <input type="url" placeholder="URL da imagem do nível" name='urlImagem${i}' >
-        <input type="text" placeholder="Descrição do nível" name='descricaoNivel${i}'>
+        <input  data-test="level-input" id='tituloNivel' type="text" placeholder="Título do nível" name='tituloNivel${i}' >
+        <input data-test="level-percent-input" type="text" placeholder="% de acerto mínima" name='porcentagemAcertos${i}' >
+        <input data-test="level-img-input" type="url" placeholder="URL da imagem do nível" name='urlImagem${i}' >
+        <input data-test="level-description-input"
+        type="text" placeholder="Descrição do nível" name='descricaoNivel${i}'>
     </div>
     `
   }
@@ -378,6 +378,7 @@ function gerarQuizz() {
       quizzAcerto.innerHTML = ''
       const cabeca = document.createElement('div')
       cabeca.setAttribute('class', 'cabeca')
+      cabeca.setAttribute("data-test", "banner")
       quizzAcerto.appendChild(cabeca)
       cabeca.innerHTML += `
   <img src="${promisse.data.image}" alt="">
@@ -391,26 +392,26 @@ function gerarQuizz() {
         contentPergunta.setAttribute('class', 'contentPergunta')
         quizzAcerto.appendChild(contentPergunta)
         contentPergunta.innerHTML += `
-      <div class="tudoPergunta">
-              <div class="perguntaQuizz" style="background-color: ${promisse.data.questions[i].color}">
+      <div data-test="question" class="tudoPergunta">
+              <div data-test="question-title"   class="perguntaQuizz" style="background-color: ${promisse.data.questions[i].color}">
                 <p>${promisse.data.questions[i].title}</p>
               </div>
               <div class="resposta ">
-                <div>
+                <div data-test="answer">
                   <img src="${promisse.data.questions[i].answers[0].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[0].text}</p>
+                  <p data-test="answer-text">${promisse.data.questions[i].answers[0].text}</p>
                 </div>
-                <div>
+                <div data-test="answer">
                   <img src="${promisse.data.questions[i].answers[1].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[1].text}</p>
+                  <p data-test="answer-text">${promisse.data.questions[i].answers[1].text}</p>
                 </div>
-                <div>
+                <div data-test="answer">
                   <img src="${promisse.data.questions[i].answers[2].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[2].text}</p>
+                  <p data-test="answer-text">${promisse.data.questions[i].answers[2].text}</p>
                 </div>
-                <div>
+                <div data-test="answer" >
                   <img src="${promisse.data.questions[i].answers[3].image}" alt="">
-                  <p>${promisse.data.questions[i].answers[3].text}</p>
+                  <p data-test="answer-text" >${promisse.data.questions[i].answers[3].text}</p>
                 </div>
               </div>
             </div>
